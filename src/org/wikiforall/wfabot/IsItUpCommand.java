06df0de95f621b3faacd4ac6d5c0d0760d211dfe
Matthew Iselin <matthew@theiselins.net>
2016-02-27 19:09:17 +1100

    added a command to figure out if a site is up or not

---
+++ src/org/wikiforall/wfabot/IsItUpCommand.java
@@ -0,0 +1,42 @@
+
+package org.wikiforall.wfabot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tells the user whether a particular page is up or not.
+ *
+ * @author Matthew Iselin <matthew@theiselins.net>
+ */
+public class IsItUpCommand implements BaseCommand {
+
+  @Override
+  public String token() {
+    return "isitup";
+  }
+
+  @Override
+  public String[] handle(String message, String from) {
+    List<String> results = new ArrayList<>();
+    for (String url : message.split(" ")) {
+      if (url.startsWith("!")) {
+        continue;
+      }
+
+      if (!url.startsWith("http")) {
+        url = "http://" + url;
+      }
+
+      String result = Downloader.download(url);
+      if (result == null) {
+        results.add(url + " appears to be down");
+      } else {
+        results.add(url + " appears to be up");
+      }
+    }
+
+    return results.toArray(new String[results.size()]);
+  }
+
+}
--- src/org/wikiforall/wfabot/PyDocCommand.java
+++ src/org/wikiforall/wfabot/PyDocCommand.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.wikiforall.wfabot;
 
 import java.io.BufferedReader;
@@ -14,8 +10,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Gets python function signatures and docstrings where available.
  *
- * @author New
+ * @author Matthew Iselin <matthew@theiselins.net>
  */
 public class PyDocCommand implements BaseCommand {
 
