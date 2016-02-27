c903b5306f2c2dc8d88d4e74cb35d34f8d026e68
Matthew Iselin <matthew@theiselins.net>
2016-02-27 15:33:34 +1100

    added multi-MdN dice rolling, still defaults to 1d6 for easy usage

--- src/org/wikiforall/wfabot/DiceCommand.java
+++ src/org/wikiforall/wfabot/DiceCommand.java
@@ -6,6 +6,10 @@
 package org.wikiforall.wfabot;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Rolls dice and sends the results.
@@ -14,11 +18,47 @@ import java.security.SecureRandom;
  */
 public class DiceCommand implements BaseCommand {
 
-  private final SecureRandom random_ = new SecureRandom();
+  private static SecureRandom random_ = new SecureRandom();
+  private static final Pattern mdn_re_ = Pattern.compile("(?<count>\\d+)??d(?<sides>\\d+)??");
 
-  public int roll(int sides) {
-    // nextInt is 0-bound inclusive, so add one to get a sane roll.
-    return random_.nextInt(sides - 1) + 1;
+  public class Dice {
+
+    private final int sides_;
+
+    public Dice(int sides) {
+      sides_ = sides;
+    }
+
+    public int getSides() {
+      return sides_;
+    }
+
+    public int roll() {
+      // nextInt is 0-bound inclusive, so add one to get a sane roll.
+      return random_.nextInt(sides_ - 1) + 1;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+      return other instanceof Dice && sides_ == ((Dice) other).sides_;
+    }
+
+    @Override
+    public int hashCode() {
+      // Netbeans generated this for me, and it seems like a reasonable hash.
+      int hash = 5;
+      hash = 79 * hash + this.sides_;
+      return hash;
+    }
+  }
+
+  public DiceCommand() {
+    // Default random_ is OK.
+  }
+
+  public DiceCommand(SecureRandom override) {
+    super();
+    random_ = override;
   }
 
   @Override
@@ -26,9 +66,54 @@ public class DiceCommand implements BaseCommand {
     return "roll";
   }
 
+  public List<Dice> parseDice(String message) {
+    List<Dice> dice = new ArrayList<>();
+    for (String entry : message.split(" ")) {
+      if (entry.startsWith("!")) {
+        continue;
+      }
+
+      Matcher m = mdn_re_.matcher(entry);
+      if (!m.matches()) {
+        continue;
+      }
+
+      String scount = m.group("count");
+      String ssides = m.group("sides");
+
+      int count = 1;
+      int sides = 6;
+
+      if (null != scount) {
+        count = Integer.parseInt(scount);
+      }
+      if (null != ssides) {
+        sides = Integer.parseInt(ssides);
+      }
+
+      for (int i = 0; i < count; i++) {
+        dice.add(new Dice(sides));
+      }
+    }
+
+    if (dice.isEmpty()) {
+      // Default to 6-sided die.
+      dice.add(new Dice(6));
+    }
+
+    return dice;
+  }
+
   @Override
   public String[] handle(String message, String from) {
-    return new String[]{String.format("rolled a %d", roll(6))};
+    List<Dice> dice = parseDice(message);
+
+    String response = String.format("rolled %d dice:", dice.size());
+    for (Dice die : dice) {
+      response += String.format(" d%d=%d", die.getSides(), die.roll());
+    }
+
+    return new String[]{response};
   }
 
 }
--- test/org/wikiforall/wfabot/DiceCommandTest.java
+++ test/org/wikiforall/wfabot/DiceCommandTest.java
@@ -5,10 +5,13 @@
  */
 package org.wikiforall.wfabot;
 
+import java.security.SecureRandom;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.mockito.Mockito;
+import org.wikiforall.wfabot.DiceCommand.Dice;
 
 /**
  *
@@ -16,27 +19,53 @@ import org.mockito.Mockito;
  */
 public class DiceCommandTest {
 
-  DiceCommand instance_;
+  /**
+   * Test of parseDice method, of class DiceCommand.
+   */
+  @Test
+  public void testParseDice() {
+    DiceCommand cmd = new DiceCommand();
+    List<Dice> dice = cmd.parseDice("1d20 2d10 3d6 d6 1d d");
+    Dice[] dice_array = dice.toArray(new Dice[dice.size()]);
 
-  public DiceCommandTest() {
-  }
+    Dice[] expected = new Dice[]{
+      // 1d20
+      cmd.new Dice(20),
+      // 2d10
+      cmd.new Dice(10),
+      cmd.new Dice(10),
+      // 3d6
+      cmd.new Dice(6),
+      cmd.new Dice(6),
+      cmd.new Dice(6),
+      // d6
+      cmd.new Dice(6),
+      // 1d
+      cmd.new Dice(6),
+      // d
+      cmd.new Dice(6)
+    };
 
-  @Before
-  public void before() {
-    instance_ = Mockito.spy(new DiceCommand());
-  }
+    assertArrayEquals("parsing MdN formats works", expected, dice_array);
+   }
 
   /**
    * Test of handle method, of class DiceCommand.
    */
   @Test
   public void testHandle() {
-    Mockito.doReturn(3).when(instance_).roll(6);
+    // We have to adjust rolls because bounds are zero-inclusive, so in this case returning 2 gives
+    // dice rolls of 3.
+    SecureRandom rand = Mockito.mock(SecureRandom.class);
+    Mockito.when(rand.nextInt(Mockito.isA(Integer.class))).thenReturn(2);
 
-    String message = "!roll";
-    String[] expResult = new String[]{"rolled a 3"};
-    String[] result = instance_.handle(message, "");
-    assertArrayEquals("rolling once works as expected", expResult, result);
+    // Build the command with the mocked SecureRandom
+    DiceCommand cmd = new DiceCommand(rand);
+
+    String message = "!roll d 1d6 1d20";
+    String[] expResult = new String[]{"rolled 3 dice: d6=3 d6=3 d20=3"};
+    String[] result = cmd.handle(message, "");
+    assertArrayEquals("end-to-end command execution", expResult, result);
   }
 
 }
