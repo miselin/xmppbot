cdf5217c3a242abb717ef60d8de040aa8443d013
Matthew Iselin <matthew@theiselins.net>
2016-03-05 10:44:54 +1100

    xmppbot: WatchlistCommand now works

--- src/org/miselin/xmppbot/StockCommand.java
+++ src/org/miselin/xmppbot/StockCommand.java
@@ -16,6 +16,33 @@ import org.apache.commons.csv.CSVRecord;
  */
 public class StockCommand implements BaseCommand {
 
+  public class StockQuote {
+
+    public String ticker;
+    public String name;
+    public double ask;
+    public double bid;
+    public double close;
+    public double open;
+  }
+
+  /**
+   * Build a StockQuote object from the given Yahoo Finance CSV record.
+   *
+   * @param record the record to build from
+   * @return a constructed StockQuote filled out from the CSV record.
+   */
+  private StockQuote fromRecord(CSVRecord record) {
+    StockQuote quote = new StockQuote();
+    quote.ticker = record.get(0);
+    quote.name = record.get(1);
+    quote.ask = Double.parseDouble(record.get(2));
+    quote.bid = Double.parseDouble(record.get(3));
+    quote.close = Double.parseDouble(record.get(4));
+    quote.open = Double.parseDouble(record.get(5));
+    return quote;
+  }
+
   @Override
   public String usage() {
     return "[ticker]+";
@@ -38,6 +65,37 @@ public class StockCommand implements BaseCommand {
     return ticker;
   }
 
+  public List<StockQuote> getQuotes(List<String> tickers) {
+    StringBuilder s = new StringBuilder("http://finance.yahoo.com/d/quotes.csv?s=");
+    for (String ticker : tickers) {
+      s.append(ticker).append("+");
+    }
+    // ticker, name, ask, bid, previous close, open
+    s.append("&f=snabpo");
+
+    List<String> messages = new ArrayList<>();
+
+    String csv = Downloader.download(s.toString());
+
+    CSVParser parser;
+    try {
+      parser = CSVParser.parse(csv, CSVFormat.RFC4180.withNullString("N/A"));
+    } catch (IOException ex) {
+      return null;
+    }
+
+    List<StockQuote> result = new ArrayList<>();
+    for (CSVRecord record : parser) {
+      StockQuote q = fromRecord(record);
+      if (null == q) {
+        continue;
+      }
+      result.add(q);
+    }
+
+    return result;
+  }
+
   public String[] handleStockCommand(String msg) {
     System.out.println("handling stock command: " + msg);
     String[] entries = msg.split(" ");
@@ -57,34 +115,16 @@ public class StockCommand implements BaseCommand {
       return new String[]{"no tickers were given (usage: '!stock CODE1 CODE2 .. CODEN')"};
     }
 
-    StringBuilder s = new StringBuilder("http://finance.yahoo.com/d/quotes.csv?s=");
-    for (String ticker : tickers) {
-      s.append(ticker).append("+");
-    }
-    // ticker, name, ask, bid, previous close, open
-    s.append("&f=snabpo");
-
-    List<String> messages = new ArrayList<>();
-
-    String csv = Downloader.download(s.toString());
-
-    CSVParser parser;
-    try {
-      parser = CSVParser.parse(csv, CSVFormat.RFC4180.withNullString("N/A"));
-    } catch (IOException ex) {
+    List<StockQuote> quotes = getQuotes(tickers);
+    if (null == quotes) {
       return new String[]{"Yahoo Finance gave a malformed response."};
     }
 
-    for (CSVRecord record : parser) {
-      String ticker = record.get(0);
-      String name = record.get(1);
-      double ask = Double.parseDouble(record.get(2));
-      double bid = Double.parseDouble(record.get(3));
-      double close = Double.parseDouble(record.get(4));
-      double open = Double.parseDouble(record.get(5));
+    List<String> messages = new ArrayList<>();
 
+    for (StockQuote sq : quotes) {
       messages.add(String.format("%s (%s) ask %.3f bid %.3f open %.3f close %.3f",
-              ticker, name, ask, bid, close, open));
+              sq.ticker, sq.name, sq.ask, sq.bid, sq.close, sq.open));
     }
 
     return messages.toArray(new String[messages.size()]);
--- src/org/miselin/xmppbot/WatchlistCommand.java
+++ src/org/miselin/xmppbot/WatchlistCommand.java
@@ -1,6 +1,10 @@
 
 package org.miselin.xmppbot;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.miselin.xmppbot.util.PersistentStorage;
+
 /**
  * Provides a mechanism for getting a set of stock quotes for a user's watchlist.
  *
@@ -15,18 +19,69 @@ public class WatchlistCommand implements BaseCommand {
 
   @Override
   public String usage() {
-    return "nothing yet";
+    return "[add TICKER1 [TICKER2 [.. TICKERN]]|del TICKER1 [TICKER2 [.. TICKERN]]|show USER]";
   }
 
   @Override
   public String description() {
-    return "nothing yet";
+    return "add or remove the given ticker from your watchlist, pass nothing to list your watchlist";
+  }
+
+  private String[] showTickers(String forwho) {
+    StockCommand cmd = new StockCommand();
+    List<String> tickers = (List<String>) PersistentStorage.get("watchlist-" + forwho);
+    if (null == tickers) {
+      return new String[]{String.format("no tickers for %s yet", forwho)};
+    }
+
+    List<StockCommand.StockQuote> quotes = cmd.getQuotes(tickers);
+    String message = String.format("tickers for %s (ticker, close):", forwho);
+    for (StockCommand.StockQuote quote : quotes) {
+      message += String.format(" %s %.3f", quote.ticker, quote.close);
+    }
+    return new String[]{message};
   }
 
   @Override
   public String[] handle(String message, String from) {
-    // TODO(miselin): pass args, add tickers to PersistentStorage, get quotes, etc
-    return new String[]{"nothing to do here yet"};
+    StockCommand cmd = new StockCommand();
+
+    String[] split = message.split(" ");
+    if (split.length == 1) {
+      // List only.
+      return showTickers(from);
+    } else if (split.length >= 3) {
+      List<String> tickers = (List<String>) PersistentStorage.get("watchlist-" + from);
+      if (null == tickers) {
+        tickers = new ArrayList<>();
+      }
+
+      boolean ok = true;
+      switch (split[1]) {
+        case "add":
+          for (int i = 2; i < split.length; i++) {
+            tickers.add(cmd.fixTicker(split[i]));
+          }
+          break;
+        case "del":
+          for (int i = 2; i < split.length; i++) {
+            tickers.remove(cmd.fixTicker(split[i]));
+          }
+          break;
+        case "show":
+          return showTickers(split[2]);
+        default:
+          ok = false;
+          break;
+      }
+
+      if (ok) {
+        PersistentStorage.set("watchlist-" + from, tickers);
+        return showTickers(from);
+      }
+    }
+
+    return new String[]{"unknown/insufficient parameters for watchlist command"};
   }
 
 }
