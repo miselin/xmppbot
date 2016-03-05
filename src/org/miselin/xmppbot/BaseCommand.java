8d13d5c5764b42bc185bfeb5962047c571130a00
Matthew Iselin <matthew@theiselins.net>
2016-03-05 16:22:47 +1100

    xmppbot: clean up BaseCommand

--- src/org/miselin/xmppbot/BaseCommand.java
+++ src/org/miselin/xmppbot/BaseCommand.java
@@ -2,7 +2,8 @@
 package org.miselin.xmppbot;
 
 /**
- * BaseCommand defines the standard interface for all commands that the bot can action.
+ * BaseCommand defines the standard interface for all commands that the bot can
+ * action.
  *
  * @author Matthew Iselin <matthew@theiselins.net>
  */
@@ -10,33 +11,31 @@ public interface BaseCommand {
 
   /**
    * Get the token that will be used to match messages to this command.
-   *
-   * @return A String to match against; the "!" is not necessary in this function.
+   * @return A String to match against; no need to add a "!".
    */
-  public abstract String token();
+  String token();
 
   /**
    * Get usage information (don't include the !token, only the parameters).
    *
    * @return Usage information.
    */
-  public abstract String usage();
+  String usage();
 
   /**
    * Get a short description of this command.
    *
    * @return Short description.
    */
-  public abstract String description();
+  String description();
 
 
   /**
-   * Handle the given message, returning any messages to transmit back to the user.
-   *
+   * Handle the given message, returning any messages to respond with.
    * @param message The message to handle, including the command token itself.
    * @param from The user that sent the message.
    * @return An array of String containing the messages to send as replies.
    */
-  public abstract String[] handle(String message, String from);
+  String[] handle(String message, String from);
 
 }
