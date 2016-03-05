c4b8b2cf81ce01776835e64f92d721d509288027
Matthew Iselin <matthew@theiselins.net>
2016-03-05 16:15:26 +1100

    xmppbot: add package-info.java for packages

---
+++ src/org/miselin/xmppbot/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the XMPPBot main class and commands.
+ *
+ * New commands should inherit from {@link org.miselin.xmppbot.BaseCommand} and
+ * have an instance added to {@link org.miselin.xmppbot.XMPPBot}'s _commands
+ * list.
+ */
+package org.miselin.xmppbot;
---
+++ src/org/miselin/xmppbot/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides common, non-XMPP-related, utilities.
+ *
+ * These tend to be small static classes that are broadly useful.
+ */
+package org.miselin.xmppbot.util;
