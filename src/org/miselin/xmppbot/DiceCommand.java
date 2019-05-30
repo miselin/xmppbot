946b87ac4db09c95603479ea011ee9ad5d298918
Matthew Iselin <matthew@theiselins.net>
2019-05-29 20:46:42 -0700

    Add keep-N-highest dice sets to the dice command

--- src/org/miselin/xmppbot/DiceCommand.java
+++ src/org/miselin/xmppbot/DiceCommand.java
@@ -7,7 +7,11 @@ package org.miselin.xmppbot;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,112 +22,227 @@ import java.util.regex.Pattern;
  */
 public class DiceCommand implements BaseCommand {
 
-  private static SecureRandom random_ = new SecureRandom();
-  private static final Pattern mdn_re_ = Pattern.compile("(?<count>\\d+)??d(?<sides>\\d+)??");
+    private static SecureRandom random_ = new SecureRandom();
+    private static final Pattern mdn_re_ = Pattern.compile("(?<count>\\d+)??d(?<sides>\\d+)??(k(?<keep>\\d+)??)?");
 
-  @Override
-  public String usage() {
-    return "[MdN]+";
-  }
-
-  @Override
-  public String description() {
-    return "roll M N-sided dice; you may specify multiple MdNs; M and S are optional (default is 6 sides).";
-  }
-
-  public class Dice {
-
-    private final int sides_;
-
-    public Dice(int sides) {
-      sides_ = sides;
-    }
-
-    public int getSides() {
-      return sides_;
-    }
-
-    public int roll() {
-      // nextInt is 0-bound inclusive, so add one to get a sane roll.
-      return random_.nextInt(sides_ - 1) + 1;
+    @Override
+    public String usage() {
+        return "[MdN(kO)]+";
     }
 
     @Override
-    public boolean equals(Object other) {
-      return other instanceof Dice && sides_ == ((Dice) other).sides_;
+    public String description() {
+        return "roll M N-sided dice; you may specify multiple MdNs; M and S are optional (default is 6 sides). Optionally specify kO to keep the highest O dice.";
+    }
+
+    public class DiceSet {
+
+        public final List<Dice> dice;
+        public final int keep;
+
+        public DiceSet(List<Dice> dicelist, int keepn) {
+            dice = dicelist;
+            if (keepn == -1) {
+                keepn = dicelist.size();
+            }
+            keep = keepn;
+        }
+
+        public DiceSet(Dice[] dicelist, int keepn) {
+            dice = Arrays.asList(dicelist);
+            if (keepn == -1) {
+                keepn = dicelist.length;
+            }
+            keep = keepn;
+        }
+
+        public DiceSet(Dice die) {
+            dice = new ArrayList<>();
+            dice.add(die);
+            keep = 1;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof DiceSet)) {
+                return false;
+            }
+
+            DiceSet ds = (DiceSet) obj;
+            if (keep != ds.keep) {
+                return false;
+            }
+
+            if (dice.size() != ds.dice.size()) {
+                return false;
+            }
+
+            for (int i = 0; i < dice.size(); i++) {
+                if (!(dice.get(i).equals(ds.dice.get(i)))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 3;
+            for (Dice die : this.dice) {
+                hash = 71 * hash + die.hashCode();
+            }
+            hash = 71 * hash + this.keep;
+            return hash;
+        }
+    }
+
+    public class Roll {
+
+        public final Dice die;
+        public final int roll;
+
+        public Roll(Dice die, int roll) {
+            this.die = die;
+            this.roll = roll;
+        }
+    }
+
+    class RollComparator implements Comparator<Roll> {
+
+        public int compare(Roll s1, Roll s2) {
+            return Integer.compare(s2.roll, s1.roll);
+        }
+    }
+
+    public class Dice {
+
+        private final int sides_;
+
+        public Dice(int sides) {
+            sides_ = sides;
+        }
+
+        public int getSides() {
+            return sides_;
+        }
+
+        public Roll roll() {
+            // nextInt is 0-bound inclusive, so add one to get a sane roll.
+            return new Roll(this, random_.nextInt(sides_ - 1) + 1);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return other instanceof Dice && sides_ == ((Dice) other).sides_;
+        }
+
+        @Override
+        public int hashCode() {
+            // Netbeans generated this for me, and it seems like a reasonable hash.
+            int hash = 5;
+            hash = 79 * hash + this.sides_;
+            return hash;
+        }
+    }
+
+    public DiceCommand() {
+        // Default random_ is OK.
+    }
+
+    public DiceCommand(SecureRandom override) {
+        super();
+        random_ = override;
     }
 
     @Override
-    public int hashCode() {
-      // Netbeans generated this for me, and it seems like a reasonable hash.
-      int hash = 5;
-      hash = 79 * hash + this.sides_;
-      return hash;
-    }
-  }
-
-  public DiceCommand() {
-    // Default random_ is OK.
-  }
-
-  public DiceCommand(SecureRandom override) {
-    super();
-    random_ = override;
-  }
-
-  @Override
-  public String token() {
-    return "roll";
-  }
-
-  public List<Dice> parseDice(String message) {
-    List<Dice> dice = new ArrayList<>();
-    for (String entry : message.split(" ")) {
-      if (entry.startsWith("!")) {
-        continue;
-      }
-
-      Matcher m = mdn_re_.matcher(entry);
-      if (!m.matches()) {
-        continue;
-      }
-
-      String scount = m.group("count");
-      String ssides = m.group("sides");
-
-      int count = 1;
-      int sides = 6;
-
-      if (null != scount) {
-        count = Integer.parseInt(scount);
-      }
-      if (null != ssides) {
-        sides = Integer.parseInt(ssides);
-      }
-
-      for (int i = 0; i < count; i++) {
-        dice.add(new Dice(sides));
-      }
+    public String token() {
+        return "roll";
     }
 
-    if (dice.isEmpty()) {
-      // Default to 6-sided die.
-      dice.add(new Dice(6));
+    public List<DiceSet> parseDice(String message) {
+        List<DiceSet> diceset = new ArrayList<>();
+        for (String entry : message.split(" ")) {
+            List<Dice> dice = new ArrayList<>();
+            if (entry.startsWith("!")) {
+                continue;
+            }
+
+            Matcher m = mdn_re_.matcher(entry);
+            if (!m.matches()) {
+                continue;
+            }
+
+            String scount = m.group("count");
+            String ssides = m.group("sides");
+            String skeep = m.group("keep");
+
+            int count = 1;
+            int sides = 6;
+            int keep = -1;
+
+            if (null != scount) {
+                count = Integer.parseInt(scount);
+            }
+            if (null != ssides) {
+                sides = Integer.parseInt(ssides);
+            }
+            if (null != skeep) {
+                keep = Integer.parseInt(skeep);
+            }
+
+            for (int i = 0; i < count; i++) {
+                dice.add(new Dice(sides));
+            }
+
+            diceset.add(new DiceSet(dice, keep));
+        }
+
+        if (diceset.isEmpty()) {
+            // Default to 6-sided die.
+            diceset.add(new DiceSet(new Dice(6)));
+        }
+
+        return diceset;
     }
 
-    return dice;
-  }
+    @Override
+    public String[] handle(String message, String from) {
+        List<DiceSet> dice = parseDice(message);
 
-  @Override
-  public String[] handle(String message, String from) {
-    List<Dice> dice = parseDice(message);
+        int i = 1;
 
-    String response = String.format("rolled %d dice:", dice.size());
-    for (Dice die : dice) {
-      response += String.format(" d%d=%d", die.getSides(), die.roll());
+        String response = String.format("rolled %d dice sets:", dice.size());
+        for (DiceSet diceset : dice) {
+            response += String.format("\nset #%d keep %d:", i, diceset.keep);
+            List<Roll> rolls = new ArrayList<>();
+            List<Roll> discarded = new ArrayList<>();
+            for (Dice die : diceset.dice) {
+                rolls.add(die.roll());
+            }
+
+            // # of dice to keep
+            Collections.sort(rolls, new RollComparator());
+            for (int n = 0; n < (diceset.dice.size() - diceset.keep); n++) {
+                discarded.add(rolls.get(0));
+                rolls.remove(0);
+            }
+
+            for (Roll roll : rolls) {
+                response += String.format(" d%d=%d", roll.die.getSides(), roll.roll);
+            }
+
+            if (discarded.size() > 0) {
+                response += " discarded ";
+                for (Roll roll : discarded) {
+                    response += String.format(" d%d=%d", roll.die.getSides(), roll.roll);
+                }
+            }
+
+            i++;
+        }
+
+        return new String[]{response};
     }
-
-    return new String[]{response};
-  }
 
 }
--- test/org/miselin/xmppbot/DiceCommandTest.java
+++ test/org/miselin/xmppbot/DiceCommandTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.mockito.Mockito;
 import org.miselin.xmppbot.DiceCommand.Dice;
+import org.miselin.xmppbot.DiceCommand.DiceSet;
 
 /**
  *
@@ -20,53 +21,53 @@ import org.miselin.xmppbot.DiceCommand.Dice;
  */
 public class DiceCommandTest {
 
-  /**
-   * Test of parseDice method, of class DiceCommand.
-   */
-  @Test
-  public void testParseDice() {
-    DiceCommand cmd = new DiceCommand();
-    List<Dice> dice = cmd.parseDice("1d20 2d10 3d6 d6 1d d");
-    Dice[] dice_array = dice.toArray(new Dice[dice.size()]);
+    /**
+     * Test of parseDice method, of class DiceCommand.
+     */
+    @Test
+    public void testParseDice() {
+        DiceCommand cmd = new DiceCommand();
+        List<DiceSet> dice = cmd.parseDice("1d20 2d10 3d6 d6 1d d 4d6k3");
+        DiceSet[] dice_array = dice.toArray(new DiceSet[dice.size()]);
 
-    Dice[] expected = new Dice[]{
-      // 1d20
-      cmd.new Dice(20),
-      // 2d10
-      cmd.new Dice(10),
-      cmd.new Dice(10),
-      // 3d6
-      cmd.new Dice(6),
-      cmd.new Dice(6),
-      cmd.new Dice(6),
-      // d6
-      cmd.new Dice(6),
-      // 1d
-      cmd.new Dice(6),
-      // d
-      cmd.new Dice(6)
-    };
+        DiceSet[] expected = new DiceSet[]{
+            // 1d20
+            cmd.new DiceSet(cmd.new Dice(20)),
+            // 2d10
+            cmd.new DiceSet(new Dice[]{cmd.new Dice(10), cmd.new Dice(10)}, -1),
+            // 3d6
+            cmd.new DiceSet(new Dice[]{cmd.new Dice(6), cmd.new Dice(6), cmd.new Dice(6)}, -1),
+            // d6
+            cmd.new DiceSet(cmd.new Dice(6)),
+            // 1d
+            cmd.new DiceSet(cmd.new Dice(6)),
+            // d
+            cmd.new DiceSet(cmd.new Dice(6)),
+            // 4d6k3
+            cmd.new DiceSet(new Dice[]{cmd.new Dice(6), cmd.new Dice(6), cmd.new Dice(6), cmd.new Dice(6)}, 3)
+        };
 
-    assertArrayEquals("parsing MdN formats works", expected, dice_array);
-   }
+        assertEquals("correct number of dice sets parsed out", dice.size(), 7);
+        assertArrayEquals("parsing MdN formats works", expected, dice_array);
+    }
 
-  /**
-   * Test of handle method, of class DiceCommand.
-   */
-  @Test
-  public void testHandle() {
-    // We have to adjust rolls because bounds are zero-inclusive, so in this case returning 2 gives
-    // dice rolls of 3.
-    SecureRandom rand = Mockito.mock(SecureRandom.class);
-    Mockito.when(rand.nextInt(Mockito.isA(Integer.class))).thenReturn(2);
+    /**
+     * Test of handle method, of class DiceCommand.
+     */
+    @Test
+    public void testHandle() {
+        // We have to adjust rolls because bounds are zero-inclusive, so in this case returning 2 gives
+        // dice rolls of 3.
+        SecureRandom rand = Mockito.mock(SecureRandom.class);
+        Mockito.when(rand.nextInt(Mockito.isA(Integer.class))).thenReturn(2);
 
-    // Build the command with the mocked SecureRandom
-    DiceCommand cmd = new DiceCommand(rand);
+        // Build the command with the mocked SecureRandom
+        DiceCommand cmd = new DiceCommand(rand);
 
-    String message = "!roll d 1d6 1d20";
-    String[] expResult = new String[]{"rolled 3 dice: d6=3 d6=3 d20=3"};
-    String[] result = cmd.handle(message, "");
-    assertArrayEquals("end-to-end command execution", expResult, result);
-  }
+        String message = "!roll d 1d6 1d20";
+        String[] expResult = new String[]{"rolled 3 dice sets:\nset #1 keep 1: d6=3\nset #2 keep 1: d6=3\nset #3 keep 1: d20=3"};
+        String[] result = cmd.handle(message, "");
+        assertArrayEquals("end-to-end command execution", expResult, result);
+    }
 
 }
