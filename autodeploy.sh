338813c4458cf622cf4c290f5a690cf419a8bd94
Matthew Iselin <matthew@theiselins.net>
2019-06-08 10:54:31 -0700

    Update autodeploy script to always start the bot if it isn't running

--- autodeploy.sh
+++ autodeploy.sh
@@ -2,6 +2,9 @@
 
 ORIG=$(sha256sum target/xmppbot-1.0-1-jar-with-dependencies.jar)
 
+# Make sure the bot is running before we rebuild it
+screen -XS wfabot true || bash ./run.sh
+
 git fetch origin
 reslog=$(git log HEAD..origin/master --oneline)
 if [[ "${reslog}" != "" ]] ; then
