ed0babbf8a55d19e0d6756cac4cac30c60225026
Matthew Iselin <matthew@theiselins.net>
2019-06-08 11:31:41 -0700

    version bump in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 screen -XS wfabot quit || echo "doesn't exist yet"
-screen -dmS wfabot java -jar $PWD/target/xmppbot-1.0-1-jar-with-dependencies.jar $PWD/config.properties
+screen -dmS wfabot java -jar $PWD/target/xmppbot-1.1-1-jar-with-dependencies.jar $PWD/config.properties
